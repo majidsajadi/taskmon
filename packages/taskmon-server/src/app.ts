df621c3804b20c8b3c1c17abe9f76e99d8a7c253
Majid Sajadi <majidsajadi93@gmail.com>
2022-02-13 14:52:25 +0330

    feat: add dotenv

--- packages/taskmon-server/src/app.ts
+++ packages/taskmon-server/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Response, Request, NextFunction, Router } from "express";
+import { config } from "dotenv";
 import compression from "compression";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import path from "path";
 import { AppError } from "./types";
 import { createProvider } from "./providers";
 
@@ -12,13 +14,19 @@ import * as workerHandler from "./handlers/worker";
 
 const app = express();
 
-app.set("port", process.env.PORT || 8000);
+const projectRoot = path.join(__dirname, "../../../");
+const clientPath = path.join(projectRoot, 'taskmon-app/dist');
+
+config({ path: path.join(projectRoot, ".env") });
+
+app.set("port", process.env.PORT || 8001);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan("short"));
 app.use(compression());
+app.use(express.static(clientPath));
 
 // TODO: read connection fro env
 const provider = createProvider("bullMQ", {
@@ -33,14 +41,11 @@ app.locals.provider = provider;
 
 const router = Router();
 
-// Queue routes
 router.get("/queues", queueHandler.getQueues);
 router.get("/queues/:queue", queueHandler.getQueue);
 router.delete("/queues/:queue", queueHandler.destroyQueue);
 router.put("/queues/:queue/pause", queueHandler.pauseQueue);
-// Worker routes
 router.get("/queues/:queue/workers", workerHandler.getWorkers);
-// Job routes
 router.get("/queues/:queue/jobs", jobHandler.getJobs);
 router.post("/queues/:queue/jobs", jobHandler.addJob);
 router.delete("/queues/:queue/jobs/:jobId", jobHandler.removeJob);
@@ -48,6 +53,9 @@ router.put("/queues/:queue/jobs/:jobId/promote", jobHandler.promoteJob);
 router.put("/queues/:queue/jobs/:jobId/retry", jobHandler.retryJob);
 
 app.use("/api/v1", router);
+app.use('/*', (req: Request, res: Response) => {
+  res.sendFile(path.join(clientPath, 'index.html'));
+});
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   console.log(error)
